c164ee0992eb4998bdc4043eb105ba50247b4b13
Vitorio Venturi <v.venturi@yahoo.com.br>
2021-09-27 13:56:33 -0300

    CoAlteraçãoe branch e e correção de lógica

--- src/main/java/com/sigabem/calculofrete/enums/PrazoTipo.java
+++ src/main/java/com/sigabem/calculofrete/enums/PrazoTipo.java
@@ -1,8 +1,8 @@
 package com.sigabem.calculofrete.enums;
 
 public enum PrazoTipo {
-    MESMO_DDD(1),
-    MESMA_UF(3),
+    MESMO_DDD(-9),
+    MESMA_UF(-7),
     UF_DIFERENTE(10);
 
     public int prazo;
