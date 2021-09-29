7eeb4cc87e085c4594bf18068e1cd17737e12ec4
Vitorio Venturi <v.venturi@yahoo.com.br>
2021-09-29 10:14:20 -0300

    criada branch de desencovolvinto e correções

---
+++ src/main/java/com/sigabem/calculofrete/exceptions/CepNaoEncontradoException.java
@@ -0,0 +1,9 @@
+package com.sigabem.calculofrete.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class CepNaoEncontradoException extends Exception{
+
+}
+
